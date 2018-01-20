9736515bcfdd380e1c489551a2d349077aec8ab5
ddrgis <kasabiankpp@gmail.com>
2018-01-20 18:29:34 +0200

    Add ManagerTests class with simple test

---
+++ Domain.Tests/ManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Core;
+using Domain.Core.Entities;
+using Domain.Core.Interfaces;
+using Domain.Core.Services;
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class ManagerTests
+    {
+        [Test]
+        public void CountSalary_NewManagerWithFewSubordinates_ReturnSalaryWithIncrement()
+        {
+            SystemTime.Set(new DateTime(2000, 1, 1));
+            IEmployee manager = new Manager
+            {
+                BaseSalary = 2000,
+                DateOfEmployment = SystemTime.Now,
+                SubordinatesIncrement = 0.5,
+                Subordinates = new List<IEmployee>
+                {
+                    new Employee(baseSalary: 1000, dateOfEmployment: SystemTime.Now),
+                    new Employee(baseSalary: 1000, dateOfEmployment: SystemTime.Now)
+                }
+            };
+
+            double salary = manager.CountSalary(SystemTime.Now);
+
+            Assert.AreEqual(2010, salary);
+        }
+    }
+}
