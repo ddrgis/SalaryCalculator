910d893806cfc440030662fa443f5355181739e4
ddrgis <kasabiankpp@gmail.com>
2018-01-20 14:27:11 +0200

    Install NUnit. Add test project

---
+++ Domain.Tests/PersonTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass("Test");
+        }
+    }
+}
