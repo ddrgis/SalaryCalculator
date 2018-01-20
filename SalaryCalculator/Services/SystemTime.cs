0414e6e1ac4a2de791c88c0f54dc6118b038e027
ddrgis <kasabiankpp@gmail.com>
2018-01-20 15:26:03 +0200

    Add SystemTime class to stub DateTime in tests

---
+++ SalaryCalculator/Services/SystemTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain.Core.Services
+{
+    public class SystemTime
+    {
+        private static DateTime _date;
+
+        public static void Set(DateTime date)
+        {
+            _date = date;
+        }
+
+        public static void Reset()
+        {
+            _date = DateTime.MinValue;
+        }
+
+        public static DateTime Now => _date != DateTime.MinValue ? _date : DateTime.Now;
+    }
+}
