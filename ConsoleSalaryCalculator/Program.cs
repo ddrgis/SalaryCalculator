b053cfe18a9ea85460970d582fbb9e816489f2d0
ddrgis <kasabiankpp@gmail.com>
2018-01-23 00:17:14 +0200

    Add EmployeeDB. Add Console project to test connection.

---
+++ ConsoleSalaryCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace ConsoleSalaryCalculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString;
+            const string sqlExpression = "SELECT * FROM Employees";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var query = new SQLiteCommand(sqlExpression, connection);
+                SQLiteDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetValue(0));
+                    Console.WriteLine(reader.GetValue(1));
+                    Console.WriteLine(reader.GetValue(2));
+                    Console.WriteLine(reader.GetValue(3));
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
