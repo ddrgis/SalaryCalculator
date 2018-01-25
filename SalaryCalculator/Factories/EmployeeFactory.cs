448fb77980b2a9c0d9fe024e37b4aa03a4f6a1e6
ddrgis <kasabiankpp@gmail.com>
2018-01-25 22:13:22 +0200

    Add IEmployee factory

---
+++ SalaryCalculator/Factories/EmployeeFactory.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Core.Interfaces;
+using System;
+
+namespace Domain.Core.Factories
+{
+    public static class EmployeeFactory
+    {
+        public static IEmployee CreateEmployee(string employeeType, params object[] args)
+        {
+            try
+            {
+                Type type = Type.GetType("Domain.Core.Entities." + employeeType);
+                return type != null ? (IEmployee)Activator.CreateInstance(type, args) : null;
+            }
+            catch (MissingMemberException)
+            {
+                return null;
+            }
+        }
+    }
+}
