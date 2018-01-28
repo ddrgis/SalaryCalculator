d8a88181171ff3aa338fdf9eb8196a01323a5089
ddrgis <kasabiankpp@gmail.com>
2018-01-28 12:55:43 +0200

    Add test of SalaryService

---
+++ Domain.Tests/SalaryServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class SalaryServiceTests
+    {
+        [TearDown]
+        public void ResetAfterEachTest()
+        {
+            SystemTime.Reset();
+        }
+
+        [Test]
+        public void GetTotalSalary_ForOneEmployee_ReturnsHisSalary()
+        {
+            SystemTime.Set(new DateTime(2000, 01, 01));
+            var stubEmployee = new List<IEmployee>()
+            {
+                new Employee(2000, SystemTime.Now)
+            };
+            var fakeEmployeeRepository = Substitute.For<IEmployeeRepository>();
+            fakeEmployeeRepository.List().Returns(stubEmployee);
+            ISalaryService service = new SalaryService(fakeEmployeeRepository);
+
+            var total = service.GetTotalSalary(SystemTime.Now);
+
+            Assert.AreEqual(2000, total);
+        }
+    }
+}
