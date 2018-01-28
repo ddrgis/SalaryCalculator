3d08a6bc3d3108f1cf875d66d398efe267de6c62
ddrgis <kasabiankpp@gmail.com>
2018-01-28 17:16:24 +0200

    Implement GetTotalSalary in SalaryService.

--- Domain.Interfaces/ISalaryService.cs
+++ Domain.Interfaces/ISalaryService.cs
@@ -4,6 +4,6 @@ namespace Domain.Interfaces
 {
     public interface ISalaryService
     {
-        double GetTotalSalary(DateTime date);
+        double GetTotalSalary(DateTime? date);
     }
 }
--- Infrastructure.Business/SalaryService.cs
+++ Infrastructure.Business/SalaryService.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Linq;
+using Domain.Core;
 using Domain.Interfaces;
+using Infrastructure.Database;
 
 namespace Infrastructure.Business
 {
     public class SalaryService : ISalaryService
     {
-        public SalaryService(IEmployeeRepository repository)
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public SalaryService(IEmployeeRepository repository = null)
         {
+            _employeeRepository = repository ?? new EmployeeRepository();
         }
-        public double GetTotalSalary(DateTime date)
+
+        public double GetTotalSalary(DateTime? date = null)
         {
-            throw new NotImplementedException();
+            if (date == null)
+            {
+                date = SystemTime.Now;
+            }
+
+            var employees = _employeeRepository.List();
+            return employees.Aggregate(0.0, ((d, employee) => employee.CountSalary(date)));
         }
     }
 }
