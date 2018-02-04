2a6503b6ff45eae923834baa51dcafbc5fa87c4f
ddrgis <kasabiankpp@gmail.com>
2018-02-04 20:18:44 +0200

    Add failed tests of SalaryService

--- Infrastructure.Business.Tests/SalaryServiceTests.cs
+++ Infrastructure.Business.Tests/SalaryServiceTests.cs
@@ -34,5 +34,52 @@ namespace Infrastructure.Business.Tests
 
             Assert.AreEqual(1000, total);
         }
+
+        [Test]
+        public void GetTotalSalary_ForFewEmployees_ReturnsTotalSalary()
+        {
+            SystemTime.Set(new DateTime(2000, 01, 01));
+            var stubEmployee = new List<IEmployee>
+            {
+                CreateDefaultEmployee(),
+                CreateDefaultEmployee(lengthOfWork: 5),
+                CreateDefaultEmployee()
+            };
+            var fakeEmployeeRepository = Substitute.For<IEmployeeRepository>();
+            fakeEmployeeRepository.List().Returns(stubEmployee);
+            ISalaryService service = new SalaryService(fakeEmployeeRepository);
+
+            double total = service.GetTotalSalary(SystemTime.Now);
+
+            Assert.AreEqual(3150, total);
+        }
+
+        [Test]
+        public void GetTotalSalary_ForFullTreelikeStructure_ReturnsTotalSalary()
+        {
+            SystemTime.Set(new DateTime(2000, 01, 01));
+            List<IEmployee> stubEmployees = CreateFullListOfEmployees();
+            var fakeEmployeeRepository = Substitute.For<IEmployeeRepository>();
+            fakeEmployeeRepository.List().Returns(stubEmployees);
+            ISalaryService service = new SalaryService(fakeEmployeeRepository);
+
+            double total = service.GetTotalSalary(SystemTime.Now);
+
+            Assert.AreEqual(40921, total, 1);
+        }
+
+        [Test]
+        public void GetTotalSalary_ForBottomPartOfTreelikeStructure_ReturnsTotalSalary()
+        {
+            SystemTime.Set(new DateTime(2000, 01, 01));
+            List<IEmployee> stubEmployees = CreateSmallListOfEmployees();
+            var fakeEmployeeRepository = Substitute.For<IEmployeeRepository>();
+            fakeEmployeeRepository.List().Returns(stubEmployees);
+            ISalaryService service = new SalaryService(fakeEmployeeRepository);
+
+            double total = service.GetTotalSalary(SystemTime.Now);
+
+            Assert.AreEqual(12155.42, total, 1);
+        }
     }
 }
