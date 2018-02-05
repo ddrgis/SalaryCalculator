37779b20b7451cb038e05042a951486c37f06146
ddrgis <kasabiankpp@gmail.com>
2018-02-05 02:07:48 +0200

    Add EmployeeController without implementation. Add Index view. Configuration of DB connection is not setup.

---
+++ Web/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Core.Interfaces;
+using Domain.Interfaces;
+using Infrastructure.Database;
+using AutoMapper;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public EmployeeController()
+        {
+            _repository = new EmployeeRepository();
+        }
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            List<IEmployee> employees = _repository.List();
+            List<EmployeeViewModel> viewModels = Mapper.Map<List<IEmployee>, List<EmployeeViewModel>>(employees);
+            return View(viewModels);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AutoMapper;
+using Domain.Core.Interfaces;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Web.Models;
 
 namespace Web
 {
@@ -16,6 +15,8 @@ namespace Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Mapper.Initialize(cfg => cfg.CreateMap<IEmployee, EmployeeViewModel>());
         }
     }
 }
